e5d9089fcb0e6e2b9ef4790e6c6c6e3c3b559cab
GetulioLT <88693489+GetulioLT@users.noreply.github.com>
2024-03-09 20:40:44 -0300

    comentários da api
    colocado os comentários da api.

--- api/app.py
+++ api/app.py
@@ -1,3 +1,4 @@
+# Importando as bibliotecas necessárias
 from apiflask import APIFlask, Schema, abort
 from apiflask.fields import Integer, String
 from apiflask.validators import Length
@@ -7,17 +8,24 @@ from flask_migrate import Migrate
 
 from flask_httpauth import HTTPBasicAuth
 
+# Inicializando a aplicação Flask
 app = APIFlask(__name__)
+# Configurando a URI do banco de dados
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.sqlite'
+# Desativando o rastreamento de modificações do SQLAlchemy
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Inicializando o SQLAlchemy e o Migrate
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Inicializando a autenticação HTTP Basic
 auth = HTTPBasicAuth()
 
+# Função para verificar a senha
 @auth.verify_password
 def verify_password(username, password):
+    # Se o usuário e a senha forem 'admin', retorna True
     if username == 'admin' and password == 'admin':
         return True
     return False
@@ -45,19 +53,23 @@ class User(db.Model):
 @app.output(UserOut)
 def cadastrar(json_data):
     try:
+        # Cria um novo usuário e o adiciona ao banco de dados
         user = User(email=json_data['email'], password=json_data['password'])
         db.session.add(user)
         db.session.commit()
         return user, 201
     except Exception as e:
+        # Se ocorrer um erro, aborta a requisição e retorna uma mensagem de erro
         return abort(400, message=f"Email já cadastrado,\n {e}")
 
 # Rota para obter informações de um usuário pelo email
 @app.get('/user/<email>')
 @app.output(UserOut)
 def get_user(email):
+    # Busca o usuário pelo email
     user = User.query.filter_by(email=email).first()
     if user is None:
+        # Se o usuário não for encontrado, retorna um erro 404
         return abort(404, message="Usuário não encontrado")
     return user, 200
 
@@ -65,9 +77,11 @@ def get_user(email):
 @app.get('/reset-db')
 @auth.login_required
 def reset_db():
+    # Apaga todas as tabelas do banco de dados e as recria
     db.drop_all()
     db.create_all()
     return "Banco de dados reiniciado com sucesso", 200
 
+# Inicia a aplicação
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
