a6cc4c67c8f492ba41182c14ddf95296d1b2a438
GetulioLT <88693489+GetulioLT@users.noreply.github.com>
2024-03-09 20:20:48 -0300

    att initi

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 # app/__init__.py
 from flask import Flask, redirect, url_for
 from flask_login import LoginManager  # Importando a classe LoginManager do flask_login
-from .models.user import User, load_user  # Importando a classe User e a função load_user
+from .models.user import load_user  # Importando a classe User e a função load_user
 
 app = Flask(__name__)  # Criando uma instância da classe Flask
 app.secret_key = "jason"  # Definindo a chave secreta da aplicação
@@ -15,6 +15,3 @@ from app.controllers import cadastro_controller, home_controller, login_controll
 @app.route('/')  # Definindo a rota raiz da aplicação
 def index():
     return redirect(url_for('login'))  # Redirecionando para a página de login
-
-if __name__ == '__main__':
-    app.run(debug=True)  # Rodando a aplicação em modo de debug
